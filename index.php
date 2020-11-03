a09f30edcd392e3bc4a3fa2b87871dbdb444a98a
Igorpnz93 <73895038+Igorpnz93@users.noreply.github.com>
2020-11-03 17:48:00 +0300

    Uploading files

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Здравствуйте';
+$name = 'Сутягин Игорь';
+$image = 'img/unknown-raccoon.svg';
+$email = 'ledpr-2019@gmail.com';
+$phone = '+79775373964';
+
+require('components/header.php');
+require('components/about.php');
+require('components/status.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
